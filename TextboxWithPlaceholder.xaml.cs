12cea43a50b79775ce87e44c60dac1f10b460a36
robin-dunn <58361313+robin-dunn@users.noreply.github.com>
2020-05-02 19:01:23 +0100

    First commit, created DbGui WPF project

---
+++ TextboxWithPlaceholder.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DbGui
+{
+	/// <summary>
+	/// Interaction logic for TextboxWithPlaceholder.xaml
+	/// </summary>
+	public partial class TextboxWithPlaceholder : UserControl
+	{
+		public TextboxWithPlaceholder()
+		{
+			InitializeComponent();
+		}
+
+		public string PlaceholderText
+		{
+			get { return (string)GetValue(PlaceholderTextProperty); }
+			set { SetValue(PlaceholderTextProperty, value); }
+		}
+
+		// Using a DependencyProperty as the backing store for Property1.  
+		// This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty PlaceholderTextProperty
+			= DependencyProperty.Register("PlaceholderText", typeof(string), typeof(TextboxWithPlaceholder));
+
+		private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			TextBox textBox = sender as TextBox;
+
+			if (textBox != null)
+			{
+				if (string.IsNullOrWhiteSpace(textBox.Text))
+				{
+					Placeholder.Visibility = Visibility.Visible;
+				}
+				else
+				{
+					Placeholder.Visibility = Visibility.Hidden;
+				}
+			}
+		}
+	}
+}
